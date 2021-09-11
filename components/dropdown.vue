cdb49fc63d2f205f8689859ef7b85bf159597d23
Sofitt <81359836+Sofitt@users.noreply.github.com>
2021-09-12 02:33:22 +0700

    Сверстал чекбоксы. Сделал отображение раскрытия только у нужных элементов списка. Изменил способ вычисления стиля для активных элементов списка. Убрал отображение чекбоксов в списках типов и сортировки. Также исправил для их активных элементов отступы и убрал возможность выбора нескольких элементов сразу (теперь работает корректно). Сделал отображение и раскрытие дочерних элементов списка. Переделал копманент со строкой поиска. Саму строку вынес в отдельный компанент. Добавил строку поиска для списка категорий.
    Начал писать логику фильтра поиска в store.
    
    БАГ. При нажатии непосредственно на checkbox, а не на label ломается логика отображения active элемента.
    БАГ. Побочным действием считывания данных со списков является визуальный сброс основного выделенного элемента.

--- components/dropdown.vue
+++ components/dropdown.vue
@@ -1,38 +1,60 @@
 <template>
   <div class="drop">
-    <button class="drop__btn" @click.prevent="changeState($event.target)">
+    <button class="drop__btn" @click.prevent="changeExpandState($event.target)">
       {{title}}
       <span class="drop__icon arrow down"></span>
     </button>
-<!--    v-if="activeState === true"-->
-    <div class="drop__list">
-      <div class="drop__item" :key="item.id" v-for="item of items">
-        <input type="checkbox" class="drop__checkbox">
-        <span class="drop__title">{{item.title}}</span>
+    <div v-if="checkboxState === true" class="drop__list">
+      <searchField :title="'Поиск по категориям'"></searchField>
+      <div class="item" :key="item.index" v-for="(item, index) of items" @click="changeItemActive('check', index);">
+        <label class="drop__item">
+          <input :id="'check'+uid+index" :value="{id: item.id, title: item.title, subtitles: []}" v-model="itemsArr" type="checkbox" class="drop__checkbox">
+          <span class="drop__title">{{item.title}}</span>
+          <span :id="'icon'+uid+index" v-if="item.subtitles" class="drop__icon arrow down" style="border-color: #C7C7C7;"></span>
+        </label>
+        <div :id="'list'+uid+index" class="sub">
+          <label v-for="(sub, index) of item.subtitles" class="drop__item" @click="changeItemActive('subCheck', index)">
+            <input :id="'subCheck'+uid+index" :value="sub" v-model="subArr" type="checkbox" class="drop__checkbox">
+            <span class="drop__title">{{sub}}</span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div :id="'list'+uid" v-if="checkboxState === false" class="drop__list short">
+      <div :id="'item'+uid+index" class="item" :key="item.index" v-for="(item, index) of items" @click="setRadio(index, uid)">
+        <label class="drop__item short">
+          <span class="drop__title short">{{item.title}}</span>
+        </label>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import searchField from "@/components/searchBar/components/searchField";
 export default {
   name: "dropdown",
-  props: ['title', 'items'],
-  components: {},
+  props: ['uid', 'isCheckbox', 'title', 'items'],
+  components: {searchField},
   data: function () {
     return {
-      activeState: false,
+      // States
+      expandState: false,
+      checkboxState: false,
+      // Arr
+      itemsArr: [],
+      subArr: [],
     }
   },
   methods: {
-    changeState(target) {
-      this.activeState = !this.activeState
+    changeExpandState(target) {
+      this.expandState = !this.expandState
       if (target.classList[0] == 'drop__icon') {
-        if (this.activeState === true) {
+        if (this.expandState === true) {
           target.classList.remove('down');
           target.classList.add('up');
           target.parentNode.classList.add('active');
-        } else if (this.activeState === false) {
+        } else if (this.expandState === false) {
           target.classList.add('down');
           target.classList.remove('up', 'active');
           target.parentNode.classList.remove('active');
@@ -40,29 +62,74 @@ export default {
       }
       else if (target.classList[0] == 'drop__btn') {
         let arrow = target.childNodes[1];
-        if (this.activeState === true) {
+        if (this.expandState === true) {
           arrow.classList.remove('down');
           arrow.classList.add('up');
           target.classList.add('active');
-        } else if (this.activeState === false) {
+        } else if (this.expandState === false) {
           arrow.classList.add('down');
           arrow.classList.remove('up');
           target.classList.remove('active');
         }
       }
 
+    },
+    changeItemActive(item, id) {
+      let checkbox;
+      let icon;
+      let subList;
+      if (item === 'check') {
+        checkbox = document.getElementById(item+this.uid+id);
+        icon = document.getElementById('icon'+this.uid+id)
+        subList = document.getElementById('list'+this.uid+id)
+      } else if (item === 'subCheck') {
+        checkbox = document.getElementById(item+this.uid+id);
+      }
+      if (checkbox.checked === true) {
+        checkbox.parentNode.classList.toggle('active')
+        if (icon) {
+          icon.classList.remove('down');
+          icon.classList.add('up');
+        }
+      if (subList) {
+        subList.style.display = 'block';
+      }
+      } else {
+        if (icon) {
+          icon.classList.add('down');
+          icon.classList.remove('up');
+        }
+        if (subList) {
+          subList.style.display = 'none';
+        }
+      }
+    },
+    setRadio(index, id) {
+      let list = document.getElementById('list'+id).childNodes;
+      let item = document.getElementById('item'+this.uid+index);
+      for (let i = 0; i < list.length; i++) {
+        list[i].classList.remove('active');
+      }
+      item.classList.add('active'); // Отсбда брать данные
     }
   },
-  computed: {},
+  computed: {
+  },
   watch: {
-    activeState(newValue) {
-      if (newValue === true) {
-
-      }
-    }
+subArr() {
+  this.$store.dispatch('searchFilter/updateSubTag', this.subArr);
+  console.log(this.subArr);
+},
+itemsArr() {
+  this.$store.commit('searchFilter/setTag', this.itemsArr);
+  console.log(this.itemsArr);
+}
   },
   mounted() {
-
+    if (this.isCheckbox) {
+      this.checkboxState = this.isCheckbox;
+    } else this.checkboxState = false;
+console.log(this.checkboxState);
   },
 }
 </script>
@@ -113,8 +180,14 @@ export default {
     box-sizing: border-box;
     box-shadow: 4px 4px 4px rgba(199, 199, 199, 0.28);
     border-radius: 4px;
+
+    &.short {
+      padding: 0;
+    }
   }
+  // Modal item
   &__item {
+    z-index: -10;
     display: flex;
     flex-flow: row;
     align-items: center;
@@ -128,9 +201,11 @@ export default {
     line-height: 16px;
     letter-spacing: 0em;
     text-align: left;
+    @include transitionAll();
+    //transition: all 0.3s ease 0s;
 
     &:first-child {
-      margin-top: 4px;
+      //margin-top: 4px; // **** Отступ надо делать у компонента search.
     }
     &:hover {
       background: rgba(241, 242, 246, 1);
@@ -138,16 +213,34 @@ export default {
       @include transitionAll();
       //transition: all 0.3s ease 0s;
     }
+    &.short {
+      padding: 16px 18px;
+    }
   }
+  &__checkbox {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+  }
+  // Item title
   &__title {
     margin-left: 12px;
+    flex: 0 1 89.5%;
+    &.short {
+      margin-left: 0;
+    }
   }
+}
+.sub {
+  display: none;
+  padding-left: 36px;
 }
 // Arrows
 .arrow {
-
   border: solid #C7C7C7;
-  border-width: 0 3px 3px 0;
+  border-width: 0 2px 2px 0;
   display: inline-block;
   padding: 3px;
   max-height: 10px;
@@ -174,4 +267,41 @@ export default {
   color: #43A5D2;
 }
 
+// Custom checkboxes
+input[type="checkbox"] {
+  appearance: none;
+  cursor: pointer;
+}
+label {
+  cursor: pointer;
+}
+input[type="checkbox"]:before {
+  border: 1px solid #DCDCDC;
+  border-radius: 5px;
+  content: "\00a0";
+  display: inline-block;
+  padding: 0;
+  height: 18.5px;
+  width: 18.5px;
+}
+input[type="checkbox"]:checked:before {
+  border: 1px solid #43A5D2;
+  border-radius: 5px;
+  background-image: url("../assets/svg/checked.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+// searchField component
+.drop__list > .searchField {
+  border-bottom: 1px solid #EBEDF5;
+  .searchField__find {
+    padding-left: 7px;
+  }
+  .searchField__input {
+    //background: red;
+    height: 50px;
+    padding-left: 45px;
+  }
+}
 </style>
---
+++ components/searchBar/components/searchField.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="searchField">
+    <img src="../../../assets/svg/search-icon.svg" alt="" class="searchField__find icon">
+    <input type="text" class="searchField__input" :placeholder="title">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "searchField",
+  props: ['title'],
+  components: {},
+  data: function () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {
+
+  },
+}
+</script>
+
+<style lang="scss">
+.searchField {
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  flex: 1 1 100%;
+  //max-width: 1068px;
+  // Icon
+  &__find {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    padding-left: 18px;
+
+    &.icon {
+      //position: absolute;
+      min-width: 24px !important;
+      min-height: 24px !important;
+    }
+  }
+  // Поле для ввода
+  &__input {
+    padding-left: 60px;
+    height: 40px;
+    flex: 1 1 100%;
+    //background: red; // Отладка****
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+    /* identical to box height, or 125% */
+    letter-spacing: 0.2px;
+
+    color: #3B3C40;
+
+    &::placeholder {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 16px;
+      letter-spacing: 0.2px;
+
+      color: #C1C3CA;
+    }
+  }
+}
+</style>
